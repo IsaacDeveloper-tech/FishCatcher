987a1369fc3c6862406d5c2cb1706d39eabd11a5
Isaac Ramirez <isaacramirezgarrido1@gmail.com>
2021-09-06 21:40:18 +0200

    implementing changes in powerup system

--- Assets/PowerUps/Scripts/PowerUp.cs
+++ Assets/PowerUps/Scripts/PowerUp.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Bonifications
+
+public enum Bonus
 {
-    Sells,
-    Gatherer,
-    Discounts,
-    Capacity
+    Add,
+    Multiplie
 }
+
 
 [CreateAssetMenu(fileName = "PowerUpData", menuName = "ScriptableObjects/PowerUp", order = 1)]
 public class PowerUp : ScriptableObject
@@ -16,7 +16,51 @@ public class PowerUp : ScriptableObject
     public string namePU;
     public float basePrice;
     public float multiplier;
+    public int numOfPowerUp;
+    public FloatType attributeToApplyBonus;
     public string description;
-    public Bonifications type;
+    public Bonus typeOfBonus;
     public Sprite icon;
+
+
+    public float GetBonus(float pnum)
+    {
+
+        switch (typeOfBonus)
+        {
+            case Bonus.Add:
+                return SetAddedBonus(pnum);
+            case Bonus.Multiplie:
+                return SetMultipliedBonus(pnum);
+            default:
+                return 0;
+        }
+
+    }
+
+    private float SetMultipliedBonus(float pnum)
+    {
+        float bonus = pnum;
+
+        for (int i = 0; i < numOfPowerUp; i++)
+        {
+            bonus *= multiplier;
+        }
+
+        return bonus;
+    }
+
+
+    private float SetAddedBonus(float pnum)
+    {
+        float bonus = pnum;
+
+        for (int i = 0; i < numOfPowerUp; i++)
+        {
+            bonus += multiplier;
+        }
+
+        return bonus > 0 ? bonus : 0;
+    }
+
 }
