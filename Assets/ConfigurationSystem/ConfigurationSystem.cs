7e0f5c8330510a473dd0ae85d5f4636f45e1da63
Isaac Ramirez <isaacramirezgarrido1@gmail.com>
2021-09-18 14:58:20 +0200

    Configuration system completed

---
+++ Assets/ConfigurationSystem/ConfigurationSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigurationSystem : MonoBehaviour
+{
+
+    private bool show;
+
+    private Vector2 hideScale;
+    private Vector2 showScale;
+
+    private float speedAnim;
+
+    public RectTransform trans;
+
+    private void Awake()
+    {
+        hideScale = new Vector3(0, 0, 0);
+        showScale = new Vector3(1, 1, 1);
+        speedAnim = .2f;
+    }
+
+    private void Update()
+    {
+        if (show)
+        {
+            trans.localScale = Vector3.Lerp(trans.localScale, showScale, speedAnim);
+        }
+        else
+        {
+            trans.localScale = Vector3.Lerp(trans.localScale, hideScale, speedAnim);
+        }
+    }
+
+    public void Show()
+    {
+        show = true;
+    }
+
+
+    public void Hidde()
+    {
+        show = false;
+    }
+
+}
---
+++ Assets/ConfigurationSystem/SoundConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundConfiguration : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+
+    public void SetMusicVolume(float pvolume)
+    {
+        mixer.SetFloat("musicVolume", pvolume);
+    }
+
+    public void SetSoundsVolume(float pvolume)
+    {
+        mixer.SetFloat("soundsVolume", pvolume);
+    }
+}
